e3fe4afa9991a28d4dc92194ac0ab26728bdd921
Carlos Miguel Patiño <carlos@factored.ai>
2024-03-18 18:23:22 +0100

    Add ROC-AUC and test set for evaluation

--- llm_classifier/main.py
+++ llm_classifier/main.py
@@ -1,7 +1,9 @@
+import json
 import os
 from string import Template
 
 import torch
+import torchmetrics
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -51,24 +53,30 @@ if __name__ == "__main__":
     train_roc_auc_hist = []
     val_roc_auc_hist = []
 
+    train_auc_history = []
+    val_auc_history = []
+
+    train_roc_auc = torchmetrics.BinaryAUROC()
+    val_roc_auc = torchmetrics.BinaryAUROC()
+
     for epoch in tqdm(range(3)):
         train_loss = 0
         for batch in tqdm(train_loader, desc="Training", leave=False):
             input_ids = tokenizer(batch["text"], return_tensors="pt", padding=True)
             outputs = model(**input_ids)
-            import pdb
-
-            pdb.set_trace()
             logits = outputs.logits[:, -1, :]
             filtered_logits = logits[:, target_tokens]
+            preds = torch.softmax(filtered_logits, dim=-1)
 
-            loss = criterion(torch.softmax(filtered_logits, dim=-1), batch["label"])
+            loss = criterion(preds, batch["label"])
             loss.backward()
 
             optimizer.step()
             optimizer.zero_grad(set_to_none=True)
 
             train_loss += loss.item()
+            train_roc_auc(preds, batch["label"])
+        train_auc_history.append(train_roc_auc.compute())
 
         with torch.no_grad():
             val_loss = 0
@@ -77,10 +85,11 @@ if __name__ == "__main__":
                 outputs = model(**input_ids)
                 logits = outputs.logits[:, -1, :]
                 filtered_logits = logits[:, target_tokens].squeeze()
+                preds = torch.softmax(filtered_logits, dim=-1)
 
-                val_loss += criterion(
-                    torch.softmax(filtered_logits, dim=-1), batch["label"]
-                ).item()
+                val_loss += criterion(preds, batch["label"]).item()
+                val_roc_auc(preds, batch["label"])
+            val_auc_history.append(val_roc_auc.compute())
 
             print(f"Epoch {epoch}: Validation Loss: {val_loss/len(val_loader)}")
 
@@ -88,3 +97,33 @@ if __name__ == "__main__":
         val_loss_hist.append(val_loss / len(val_loader))
         torch.save(model.state_dict(), f"model_{epoch}.pt")
         print(f"Epoch {epoch}: Train Loss: {train_loss/len(train_loader)}")
+
+    test_roc_auc = torchmetrics.BinaryAUROC()
+
+    with torch.no_grad():
+        test_loss = 0
+        for batch in tqdm(test_loader, desc="Testing", leave=False):
+            input_ids = tokenizer(batch["text"], return_tensors="pt", padding=True)
+            outputs = model(**input_ids)
+            logits = outputs.logits[:, -1, :]
+            filtered_logits = logits[:, target_tokens].squeeze()
+            preds = torch.softmax(filtered_logits, dim=-1)
+
+            test_loss += criterion(preds, batch["label"]).item()
+            test_roc_auc(torch.softmax(filtered_logits, dim=-1), batch["label"])
+
+        print(f"Test AUC: {test_roc_auc.compute()}")
+        print(f"Test Loss: {test_loss/len(test_loader)}")
+
+    with open("metrics.json", "w") as f:
+        json.dump(
+            {
+                "train_loss": train_loss_hist,
+                "val_loss": val_loss_hist,
+                "test_loss": test_loss / len(test_loader),
+                "train_auc": train_auc_history,
+                "val_auc": val_auc_history,
+                "test_auc": test_roc_auc.compute(),
+            },
+            f,
+        )
